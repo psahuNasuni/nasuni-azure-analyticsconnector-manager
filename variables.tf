71a901b38153078cc91afe3c9a30833ac9a8e257
Pradeepta Sahu <psahu@nasuni.com>
2023-05-08 16:25:03 +0530

    public flow changes: added default value for search_outbound_subnet

--- variables.tf
+++ variables.tf
@@ -96,7 +96,7 @@ variable "use_private_ip" {
 }
 
 variable "search_outbound_subnet" {
-  description = "Available subnet name in Virtual Network for outbound traffic integration"
+  description = "Available subnet name in Virtual Network for outbound traffic integration."
   type        = string
   default     = ""
 }
