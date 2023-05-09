bb2c2a84f291202da80e351391f550306f320d25
abedareNasuni <101194763+abedareNasuni@users.noreply.github.com>
2023-05-09 14:08:52 +0530

    Update VM Size

--- main.tf
+++ main.tf
@@ -141,7 +141,7 @@ resource "azurerm_linux_virtual_machine" "NACScheduler" {
   network_interface_ids = [
     var.use_private_ip != "Y" ? azurerm_network_interface.nac_scheduler_nic_public[0].id : azurerm_network_interface.nac_scheduler_nic_private[0].id
   ]
-  size = "Standard_F16s_v2"
+  size = "Standard_DS3_v2"
 
   os_disk {
     name                 = "NACScheduler_Disk-${random_id.unique_sg_id.dec}"
