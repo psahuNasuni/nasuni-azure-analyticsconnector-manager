686afb15c432566e3f09f72eac0a4ea5f96eaaa4
abedareNasuni <101194763+abedareNasuni@users.noreply.github.com>
2023-06-13 16:54:07 +0530

    Update offer of image

--- main.tf
+++ main.tf
@@ -151,7 +151,7 @@ resource "azurerm_linux_virtual_machine" "NACScheduler" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
+    offer     = "0001-com-ubuntu-server-focal"
     sku       = "20_04-lts-gen2"
     version   = "latest"
   }
